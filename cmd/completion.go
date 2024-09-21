dc3011c776dd42e3151341fc9f4dd045afb6b451
Sergey Makarichev <harleymack@mail.ru>
2024-09-21 22:30:03 +0300

    Small refactoring

--- cmd/completion.go
+++ cmd/completion.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -10,7 +7,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// completionCmd represents the completion command
 var completionCmd = &cobra.Command{
 	Use:   "completion [bash|zsh|fish|powershell]",
 	Short: "Generate completion script",
